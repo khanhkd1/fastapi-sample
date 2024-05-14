97505b9682a9746641dfe8d96c2631ed1823713a
Khanh Kieu Duy <51400512+khanhkd1@users.noreply.github.com>
2024-05-14 08:17:54 +0000

    edit docker-compose

--- app/database.py
+++ app/database.py
@@ -8,9 +8,10 @@ mysql_user = os.environ.get('MYSQL_USER')
 mysql_password = os.environ.get('MYSQL_PASSWORD')
 mysql_host = os.environ.get('MYSQL_HOST')
 mysql_db = os.environ.get('MYSQL_DATABASE')
+mysql_port = os.environ.get('MYSQL_PORT')
 
 engine = create_engine(
-    f"postgresql+psycopg2://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"
+    f"postgresql+psycopg2://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_db}"
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
